653628263e659ef104d3184e16f07cdb906574b2
Bhawesh Kurmi <bhaweshkurmi.eckit@gmail.com>
2017-04-12 11:56:21 +0530

    Exception Handling for Login

--- UserAccountAPI/src/main/java/com/wipro/usecase/respository/UserAccountDao.java
+++ UserAccountAPI/src/main/java/com/wipro/usecase/respository/UserAccountDao.java
@@ -2,18 +2,25 @@ package com.wipro.usecase.respository;
 
 import java.util.Date;
 
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import com.wipro.usercase.models.ErrorMessage;
 import com.wipro.usercase.models.PersonalDetails;
 
 public class UserAccountDao {
 
 	public PersonalDetails getDetailsIfUserExists(String userName, String password) {
-		
-		if("bkurmi".equalsIgnoreCase(userName) && "123456".equalsIgnoreCase(password)){
-			return new PersonalDetails("Bhawesh Kurmi", "D-102, montvert Tropez, Kaspatewasti", new Date(), "Pune", "Maharashtra", "India");
-		}else{
-			return null;
+		if ("bkurmi".equalsIgnoreCase(userName) && "123456".equalsIgnoreCase(password)) {
+			return new PersonalDetails("Bhawesh Kurmi", "D-102, montvert Tropez, Kaspatewasti", new Date(), "Pune",
+					"Maharashtra", "India");
+		} else {
+			ErrorMessage errorMessage = new ErrorMessage(404, "You have Entered wrong id and Password");
+			Response response = Response.status(Status.NOT_FOUND).entity(errorMessage).build();
+			throw new WebApplicationException(response);
 		}
-		
+
 	}
 
 }
