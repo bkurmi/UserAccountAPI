3fb5db4774aef145645e9dbe4435e174b08c711a
Bhawesh Kurmi <bhaweshkurmi.eckit@gmail.com>
2017-04-13 13:35:24 +0530

    Added Utility Class

--- UserAccountAPI/src/main/java/com/wipro/usecase/resources/MyResource.java
+++ UserAccountAPI/src/main/java/com/wipro/usecase/resources/MyResource.java
@@ -8,16 +8,17 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
 
 import com.wipro.usecase.models.PersonalDetails;
 import com.wipro.usecase.services.UserAccountServices;
+import com.wipro.usecase.util.Utility;
 
 /**
  * Root resource (exposed at "myresource" path)
  */
-@Path("myresource")
+@Path("users")
 public class MyResource {
 
 	UserAccountServices service = new UserAccountServices();
@@ -34,27 +35,14 @@ public class MyResource {
 	public Response getDetailsIfUserExists(@PathParam(value = "userName") String userName,
 			@PathParam(value = "password") String password, @Context UriInfo uriInfo) {
 		PersonalDetails details = service.getDetailsIfUserExists(userName, password);
-		String uri = getUrilForSelf(uriInfo, details);
+		String uri = Utility.getUriForSelf(uriInfo, details);
 		details.addLink(uri, "self");
 		return Response.status(Status.ACCEPTED).entity(details).header("Access-Control-Allow-Origin", "*").build();
 	}
 
-	private String getUrilForSelf(UriInfo uriInfo, PersonalDetails details) {
-		String uri = uriInfo.getBaseUriBuilder()
-			.path(MyResource.class)
-			.path(details.getUserName())
-			.path(details.getPassword())
-			.build()
-			.toString();
-		return uri;
-	}
 
-//	@GET
-//	@Path(value = "/{userName}/account/{accountId}")
-//	public void getAccountDetails(@PathParam(value = "userName") String userName) {
-//		service.getAccountDetails(userName);
-//	}
-//	
+	//Add Test Data in table
+	
 	@POST
 	@Path(value = "/addDetails")
 	public void addUserDetails() {
---
+++ UserAccountAPI/src/main/java/com/wipro/usecase/util/Utility.java
@@ -0,0 +1,16 @@
+package com.wipro.usecase.util;
+
+import javax.ws.rs.core.UriInfo;
+
+import com.wipro.usecase.models.PersonalDetails;
+import com.wipro.usecase.resources.MyResource;
+
+public class Utility {
+
+	public static String getUriForSelf(UriInfo uriInfo, PersonalDetails details) {
+		String uri = uriInfo.getBaseUriBuilder().path(MyResource.class).path(details.getUserName())
+				.path(details.getPassword()).build().toString();
+		return uri;
+	}
+
+}
